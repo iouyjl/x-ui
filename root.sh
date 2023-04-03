84e01309cb0c1e008f860645f39db22721392b94
iouyjl <93931701+iouyjl@users.noreply.github.com>
2023-04-03 23:25:14 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-
+red='\033[0;31m'
+bblue='\033[0;34m'
+plain='\033[0m'
+red(){ echo -e "\033[31m\033[01m$1\033[0m";}
+green(){ echo -e "\033[32m\033[01m$1\033[0m";}
+yellow(){ echo -e "\033[33m\033[01m$1\033[0m";}
+white(){ echo -e "\033[37m\033[01m$1\033[0m";}
+readp(){ read -p "$(yellow "$1")" $2;}
+clear
 [[ $EUID -ne 0 ]] && su='sudo' 
 lsattr /etc/passwd /etc/shadow >/dev/null 2>&1
 chattr -i /etc/passwd /etc/shadow >/dev/null 2>&1
