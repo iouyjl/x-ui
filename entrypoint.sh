01560d49137b15b97b40d2ab3f8623a99be43327
iouyjl <93931701+iouyjl@users.noreply.github.com>
2024-01-02 01:53:31 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+
+DEFAULT_START_PORT=20000                         #默认起始端口
+DEFAULT_SOCKS_USERNAME="userb"                   #默认socks账号
+DEFAULT_SOCKS_PASSWORD="passwordb"               #默认socks密码
+PATH="/ws"                            #默认ws路径
+uuid="059ab893-7a38-4a01-a4fa-8111bb7e50cb" #默认随机UUID
+
+ARGO=($(hostname -I))
+
 apt update && apt install -y supervisor wget unzip iproute2
 wget -O m.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
 unzip m.zip
@@ -13,11 +22,7 @@ base64 -d config > config.yaml; ./$xpid -config=config.yaml
 
 
 # argo与加密方案出自fscarmen
-wget -N https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
-chmod +x cloudflared-linux-amd64
-./cloudflared-linux-amd64 tunnel --url http://localhost:8080 --no-autoupdate > argo.log 2>&1 &
-sleep 5
-ARGO=$(cat argo.log | grep -oE "https://.*[a-z]+cloudflare.com" | sed "s#https://##")
+
 xver=`./$xpid version | sed -n 1p | awk '{print $2}'`
 UA_Browser="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36"
 v4=$(curl -s4m6 ip.sb -k)
@@ -34,13 +39,13 @@ Argo_xray_vmess="vmess://$(echo -n "\
 \"net\": \"ws\",\
 \"type\": \"none\",\
 \"host\": \"${ARGO}\",\
-\"path\": \"/$uuid-vm\",\
+\"path\": \"/$PATH\",\
 \"tls\": \"tls\",\
 \"sni\": \"${ARGO}\"\
 }"\
     | base64 -w 0)" 
-Argo_xray_vless="vless://${uuid}@${ARGO}:443?encryption=none&security=tls&sni=$ARGO&type=ws&host=${ARGO}&path=/$uuid-vl#Argo_xray_vless"
-Argo_xray_trojan="trojan://${uuid}@${ARGO}:443?security=tls&type=ws&host=${ARGO}&path=/$uuid-tr&sni=$ARGO#Argo_xray_trojan"
+Argo_xray_vless="vless://${uuid}@${ARGO}:443?encryption=none&security=tls&sni=$ARGO&type=ws&host=${ARGO}&path=/$PATH#Argo_xray_vless"
+Argo_xray_trojan="trojan://${uuid}@${ARGO}:443?security=tls&type=ws&host=${ARGO}&path=/$PATH&sni=$ARGO#Argo_xray_trojan"
 
 cat > log << EOF
 ****************************************************************
