50cfcd3ab8cb166b1f554fb41388ac40da621ba3
iouyjl <93931701+iouyjl@users.noreply.github.com>
2024-01-02 03:10:37 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,69 +7,156 @@ DEFAULT_WS_PATH="/ws"                            #默认ws路径
 DEFAULT_UUID="059ab893-7a38-4a01-a4fa-8111bb7e50cb" #默认随机UUID
 
 
-
 apt update && apt install -y supervisor wget unzip iproute2
 wget -O m.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
-unzip m.zip
-chmod a+x xray
-sed -i "s/uuid/$uuid/g" ./config.yaml
-xpid=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
-mv xray $xpid
-cat config.yaml | base64 > config
-rm -f config.yaml
+unzip Xray-linux-64.zip
+mv xray /usr/local/bin/xrayLL
+chmod +x /usr/local/bin/xrayLL
+cat <<EOF >/etc/systemd/system/xrayLL.service
+[Unit]
+Description=xrayLL Service
+After=network.target
 
-base64 -d config > config.yaml; ./$xpid -config=config.yaml
+[Service]
+ExecStart=/usr/local/bin/xrayLL -c /etc/xrayLL/config.yaml
+Restart=on-failure
+User=nobody
+RestartSec=3
+
+[Install]
+WantedBy=multi-user.target
+EOF
+	systemctl daemon-reload
+	systemctl enable xrayLL.service
+	systemctl start xrayLL.service
+	echo "Xray 安装完成."
 
 
 # argo与加密方案出自fscarmen
+mkdir -p /etc/xrayLL
 
-xver=`./$xpid version | sed -n 1p | awk '{print $2}'`
+config_content+=inbounds:\n
+config_content+=- port: 10000\n
+config_content+=  protocol: vless\n
+config_content+=  settings:\n
+config_content+=    decryption: none\n
+config_content+=    clients:\n
+config_content+=    - id: ${UUID:-$DEFAULT_UUID}\n
+config_content+=  streamSettings:\n
+config_content+=    network: ws\n
+config_content+=    wsSettings:\n
+config_content+=      path: ${WS_PATH:-$DEFAULT_WS_PATH}\n
+config_content+=  sniffing:\n
+config_content+=    enabled: true\n
+config_content+=    destOverride:\n
+config_content+=    - http\n
+config_content+=    - tls\n
+config_content+=    - quic\n
+config_content+=- port: 20000\n
+config_content+=  protocol: vmess\n
+config_content+=  settings:\n
+config_content+=    clients:\n
+config_content+=    - id: ${UUID:-$DEFAULT_UUID}\n
+config_content+=  streamSettings:\n
+config_content+=    network: ws\n
+config_content+=    wsSettings:\n
+config_content+=      path: ${WS_PATH:-$DEFAULT_WS_PATH}\n
+config_content+=  sniffing:\n
+config_content+=    enabled: true\n
+config_content+=    destOverride:\n
+config_content+=    - http\n
+config_content+=    - tls\n
+config_content+=    - quic\n
+config_content+=- port: 40000\n
+config_content+=  protocol: shadowsocks\n
+config_content+=  settings:\n
+config_content+=    password: "$SOCKS_PASSWORD"\n
+config_content+=    method: chacha20-ietf-poly1305\n
+config_content+=    ivcheck: true\n
+config_content+=  streamSettings:\n
+config_content+=    network: ws\n
+config_content+=    wsSettings:\n
+config_content+=      path: ${WS_PATH:-$DEFAULT_WS_PATH}\n
+config_content+=  sniffing:\n
+config_content+=    enabled: true\n
+config_content+=    destOverride:\n
+config_content+=    - http\n
+config_content+=    - tls\n
+config_content+=    - quic\n
+config_content+=- port: 30000\n
+config_content+=  protocol: trojan\n
+config_content+=  settings:\n
+config_content+=    clients:\n
+config_content+=    - password: "$SOCKS_PASSWORD"\n
+config_content+=  streamSettings:\n
+config_content+=    network: ws\n
+config_content+=    wsSettings:\n
+config_content+=      path: ${WS_PATH:-$DEFAULT_WS_PATH}\n
+config_content+=  sniffing:\n
+config_content+=    enabled: true\n
+config_content+=    destOverride:\n
+config_content+=    - http\n
+config_content+=    - tls\n
+config_content+=    - quic\n
+config_content+=- port: 50000\n
+config_content+=  protocol: socks\n
+config_content+=  settings:\n
+config_content+=    auth: password\n
+config_content+=    accounts:\n
+config_content+=    - user: "$SOCKS_USERNAME"\n
+config_content+=      pass: "$SOCKS_PASSWORD"\n
+config_content+=  streamSettings:\n
+config_content+=    network: ws\n
+config_content+=    wsSettings:\n
+config_content+=      path: ${WS_PATH:-$DEFAULT_WS_PATH}\n
+config_content+=  sniffing:\n
+config_content+=    enabled: true\n
+config_content+=    destOverride:\n
+config_content+=    - http\n
+config_content+=    - tls\n
+config_content+=    - quic\n
+config_content+=outbounds:\n
+config_content+=- protocol: freedom\n
+config_content+=  tag: direct\n\n\n
+
+
+echo -e "$config_content" >/etc/xrayLL/config.yaml
+systemctl restart xrayLL.service
+systemctl --no-pager status xrayLL.service
 UA_Browser="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36"
 v4=$(curl -s4m6 ip.sb -k)
 v4l=`curl -sm6 --user-agent "${UA_Browser}" http://ip-api.com/json/$v4?lang=zh-CN -k | cut -f2 -d"," | cut -f4 -d '"'`
 UUID=${UUID:-$DEFAULT_UUID}
 WS_PATH=${WS_PATH:-$DEFAULT_WS_PATH}
-ARGO=$v4
+
 Argo_xray_vmess="vmess://$(echo -n "\
 {\
 \"v\": \"2\",\
-\"ps\": \"Argo_xray_vmess\",\
-\"add\": \"${ARGO}\",\
-\"port\": \"443\",\
-\"id\": \"$uuid\",\
+\"ps\": \"${v4}\",\
+\"add\": \"${v4}\",\
+\"port\": \"20000\",\
+\"id\": \"$UUID\",\
 \"aid\": \"0\",\
 \"net\": \"ws\",\
 \"type\": \"none\",\
-\"host\": \"${ARGO}\",\
+\"host\": \"\",\
 \"path\": \"/$WS_PATH\",\
 \"tls\": \"tls\",\
 \"sni\": \"${ARGO}\"\
 }"\
     | base64 -w 0)" 
-Argo_xray_vless="vless://${uuid}@${ARGO}:443?encryption=none&security=tls&sni=$ARGO&type=ws&host=${ARGO}&path=/$WS_PATH#Argo_xray_vless"
-Argo_xray_trojan="trojan://${uuid}@${ARGO}:443?security=tls&type=ws&host=${ARGO}&path=/$WS_PATH&sni=$ARGO#Argo_xray_trojan"
+Argo_xray_vless="vless://${UUID}@${v4}:443?encryption=none&security=tls&sni=$v4&type=ws&host=${v4}&path=/$WS_PATH#Argo_xray_vless"
+Argo_xray_trojan="trojan://${UUID}@${v4}:443?security=tls&type=ws&host=${v4}&path=/$WS_PATH&sni=$v4#Argo_xray_trojan"
 
 cat > log << EOF
-****************************************************************
-相关教程解读，请关注：甬哥侃侃侃
-视频教程：https://www.youtube.com/@ygkkk
-博客地址：https://ygkkk.blogspot.com
 ================================================================
-当前已安装的Xray正式版本：$xver
 当前网络的IP：$v4
 IP归属地区：$v4l
 ================================================================
-注意：重构或重启当前平台，Argo服务器地址将重置更新
-Cloudflared Argo 隧道模式Xray五协议配置如下：
-================================================================
-----------------------------------------------------------------
 1：Vmess+ws+tls配置明文如下，相关参数可复制到客户端
-Argo服务器临时地址（可更改为CDN自选IP）：$ARGO
-https端口：可选443、2053、2083、2087、2096、8443，tls必须开启
-http端口：可选80、8080、8880、2052、2082、2086、2095，tls必须关闭
-uuid：$uuid
+uuid：$UUID
 传输协议：ws
-host/sni：$ARGO
+host/sni：$v4
 path路径：/$WS_PATH
 
 分享链接如下（默认443端口、tls开启，服务器地址可更改为自选IP）
@@ -77,12 +164,9 @@ ${Argo_xray_vmess}
 
 ----------------------------------------------------------------
 2：Vless+ws+tls配置明文如下，相关参数可复制到客户端
-Argo服务器临时地址（可更改为CDN自选IP）：$ARGO
-https端口：可选443、2053、2083、2087、2096、8443，tls必须开启
-http端口：可选80、8080、8880、2052、2082、2086、2095，tls必须关闭
-uuid：$uuid
+uuid：$UUID
 传输协议：ws
-host/sni：$ARGO
+host/sni：$v4
 path路径：/$WS_PATH
 
 分享链接如下（默认443端口、tls开启，服务器地址可更改为自选IP）
@@ -90,12 +174,9 @@ ${Argo_xray_vless}
 
 ----------------------------------------------------------------
 3：Trojan+ws+tls配置明文如下，相关参数可复制到客户端
-Argo服务器临时地址（可更改为CDN自选IP）：$ARGO
-https端口：可选443、2053、2083、2087、2096、8443，tls必须开启
-http端口：可选80、8080、8880、2052、2082、2086、2095，tls必须关闭
-密码：$uuid
+密码：$UUID
 传输协议：ws
-host/sni：$ARGO
+host/sni：$v4
 path路径：/$WS_PATH
 
 分享链接如下（默认443端口、tls开启，服务器地址可更改为自选IP）
@@ -103,24 +184,18 @@ ${Argo_xray_trojan}
 
 ----------------------------------------------------------------
 4：Shadowsocks+ws+tls配置明文如下，相关参数可复制到客户端
-Argo服务器临时地址（可更改为CDN自选IP）：$ARGO
-https端口：可选443、2053、2083、2087、2096、8443，tls必须开启
-http端口：可选80、8080、8880、2052、2082、2086、2095，tls必须关闭
-密码：$uuid
+密码：$UUID
 加密方式：chacha20-ietf-poly1305
 传输协议：ws
-host/sni：$ARGO
+host/sni：$v4
 path路径：/$WS_PATH
 
 ----------------------------------------------------------------
 5：Socks+ws+tls配置明文如下，相关参数可复制到客户端
-Argo服务器临时地址（可更改为CDN自选IP）：$ARGO
-https端口：可选443、2053、2083、2087、2096、8443，tls必须开启
-http端口：可选80、8080、8880、2052、2082、2086、2095，tls必须关闭
-用户名：$uuid
-密码：$uuid
+用户名：$UUID
+密码：$UUID
 传输协议：ws
-host/sni：$ARGO
+host/sni：$v4
 path路径：/$WS_PATH
 
 
